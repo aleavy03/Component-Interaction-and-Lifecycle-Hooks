00db37da16e9c63499d1379e67d763b40e3f0979
aleavy03 <142447733+aleavy03@users.noreply.github.com>
2024-02-26 08:36:58 -0600

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ParentComponent } from './parent.component';
+import { ChildComponent } from './child.component';
+
+@NgModule({
+  declarations: [ParentComponent, ChildComponent],
+  imports: [BrowserModule],
+  bootstrap: [ParentComponent],
+})
+export class AppModule {}
